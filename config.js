2c98a22e78a6a3db1fb230728fedada8edac974e
Kawsar-A-Tonmoy <kawsar.4hm3d@gmail.com>
2025-08-16 13:33:51 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYzuaMcH_fH43e9qgdntIH7ez29wVpzaU",
+  authDomain: "thegeeks-1b8aa.firebaseapp.com",
+  projectId: "thegeeks-1b8aa",
+  storageBucket: "thegeeks-1b8aa.firebasestorage.app",
+  messagingSenderId: "662440200085",
+  appId: "1:662440200085:web:f6d8ecfdc1f4b6d23a189c"
+};
